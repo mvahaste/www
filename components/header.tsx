0cb0ccb9f63728569276262e51560eabe4cd44b8
mvahaste <mvahaste@proton.me>
2025-03-21 17:15:33 +0200

    Thicker header

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export default function Header() {
   return (
-    <header className="bg-background supports-[backdrop-filter]:bg-background/75 border-b-foreground/10 sticky top-0 z-50 flex h-16 w-full flex-row items-center border-b backdrop-blur-lg">
+    <header className="bg-background supports-[backdrop-filter]:bg-background/75 border-b-foreground/10 sticky top-0 z-50 flex h-[4.5rem] w-full flex-row items-center border-b backdrop-blur-lg">
       <div className="mx-auto flex w-full max-w-[70ch] flex-row items-center justify-between p-4">
         <div className="flex flex-row items-center gap-3">
           <Avatar>
