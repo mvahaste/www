abf81e4c5eb9b12513e8f8fae9b8b33055d62b4a
mvahaste <mvahaste@proton.me>
2025-03-21 15:28:26 +0200

    Initial setup

---
+++ components/footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer className="text-muted-foreground w-full p-4 pt-8 text-center text-sm">
+      <p>
+        View the source code on{" "}
+        <a
+          className="text-foreground font-semibold hover:underline"
+          href="https://github.com/mvahaste/portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </p>
+    </footer>
+  );
+}
