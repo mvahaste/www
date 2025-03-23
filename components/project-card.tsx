8922d1396e41fc3f35f5a9c6a722d25b64d44d6e
mvahaste <mvahaste@proton.me>
2025-03-23 14:52:10 +0200

    Improved project card

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -16,15 +16,15 @@ export default function ProjectCard({ project }: ProjectCardProps) {
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5, ease: "easeInOut" }}
       viewport={{ once: true }}
-      className="bg-card text-card-foreground flex flex-col gap-1 rounded-xl border p-6 text-sm"
+      className="bg-card text-card-foreground flex flex-col gap-2 rounded-xl border p-6 text-sm"
     >
       {/* Project image */}
-      <>
+      <div className="mb-2 aspect-[16/9] overflow-hidden rounded-md border">
         {project.image.light && project.image.dark ? (
           <>
             {/* Light theme image */}
             <Image
-              className="mb-2 aspect-[16/9] h-auto w-full rounded-sm object-cover dark:hidden"
+              className="aspect-[16/9] h-auto w-full object-cover dark:hidden"
               width={500}
               height={300}
               src={project.image.light}
@@ -33,7 +33,7 @@ export default function ProjectCard({ project }: ProjectCardProps) {
 
             {/* Dark theme image */}
             <Image
-              className="mb-2 hidden aspect-[16/9] h-auto w-full rounded-sm object-cover dark:block"
+              className="hidden aspect-[16/9] h-auto w-full object-cover dark:block"
               width={500}
               height={300}
               src={project.image.dark}
@@ -43,27 +43,27 @@ export default function ProjectCard({ project }: ProjectCardProps) {
         ) : (
           // Fallback, use whichever image is available
           <Image
-            className="mb-2 aspect-[16/9] h-auto w-full rounded-sm object-cover"
+            className="aspect-[16/9] h-auto w-full object-cover"
             width={500}
             height={300}
             src={(project.image.light || project.image.dark)!}
             alt="Project thumbnail"
           />
         )}
-      </>
+      </div>
       <h3 className="text-base font-semibold">{project.title}</h3>
       <p>{project.description}</p>
-      <div className="mt-3 flex flex-wrap gap-2">
+      <div className="mt-2 flex flex-wrap gap-2">
         {project.stack.map((s: string) => (
           <span
             key={s}
-            className="bg-muted rounded-full px-2 py-1 text-xs font-semibold"
+            className="bg-muted text-muted-foreground rounded-full px-2.5 py-1 text-xs font-semibold"
           >
             {s}
           </span>
         ))}
       </div>
-      <div className="mt-4 grid flex-grow grid-cols-2 items-end gap-4">
+      <div className="mt-3 grid flex-grow grid-cols-2 items-end gap-4">
         <Button asChild disabled={!project.url}>
           <a
             href={project.url}
